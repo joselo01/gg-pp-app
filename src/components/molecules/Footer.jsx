91082046e282575d4e1c658f6a47ace087df6d15
Bryan Jorge <bjorge@ideasg.org>
2021-09-17 12:23:16 -0500

    Carousel modified

--- src/components/molecules/Footer.jsx
+++ src/components/molecules/Footer.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Carousel } from "../organisms/Carousel";
+import c1 from "../../images/logo_empresas/AgroAurora.jpg";
+import c2 from "../../images/logo_empresas/Agroazucar.jpg";
+import c3 from "../../images/logo_empresas/Andina.jpg";
 
 
 export const Footer = () => {
@@ -24,7 +27,23 @@ export const Footer = () => {
         </div>
         <div className="footer-box-bottom">
           <div className="container">
-            <Carousel/>
+          <Carousel
+            show={5}
+          >
+                <img src={c1} alt="placeholder" />
+                <img src={c2} alt="placeholder" />
+                <img src={c3} alt="placeholder" />
+                <img src={c1} alt="placeholder" />
+                <img src={c2} alt="placeholder" />
+                <img src={c3} alt="placeholder" />
+                <img src={c1} alt="placeholder" />
+                <img src={c2} alt="placeholder" />
+                <img src={c3} alt="placeholder" />
+                <img src={c1} alt="placeholder" />
+                <img src={c2} alt="placeholder" />
+                <img src={c3} alt="placeholder" />
+            
+            </Carousel>
           </div>
         </div>
       </footer>
