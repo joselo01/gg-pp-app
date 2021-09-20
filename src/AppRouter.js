db07eb6c22644009eab6f170670669c762b405db
joselo01 <sjmujica@gmail.com>
2021-09-20 17:55:34 -0500

    upload form pre registro

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { CreatePassword } from "./pages/CreatePassword.jsx";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+import { VerificacionProveedor } from "./pages/VerificacionProveedor";
 
 export const AppRouter = () => {
   return (
@@ -10,6 +12,8 @@ export const AppRouter = () => {
         <Switch>
             <Route exact path="/" component={ Home } />
             <Route exact path="/login" component={ Login } />
+            <Route extac path="/supplier_verification" component={ VerificacionProveedor } />
+            <Route extac path="/create_password" component={ CreatePassword } />
         </Switch>
       </>
     </Router>
---
+++ src/pages/CreatePassword.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Logo } from "../components/atoms/Logo";
+import { Footer } from "../components/molecules/Footer";
+
+export const CreatePassword = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <Logo />
+          </div>
+          <div className="col-6"></div>
+
+          <div className="content-box-internas mt-5 col-12">
+            <form>
+              <div className="col-12">
+                <div className="input-group">
+                  <span className="input-group-text">Crear contraseña</span>
+                  <input
+                    type="password"
+                    aria-label="First name"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div className="col-12">
+                <div className="input-group mt-2">
+                  <span className="input-group-text">Confirmar contraseña</span>
+                  <input
+                    type="password"
+                    aria-label="First name"
+                    className="form-control"
+                  />
+                </div>
+              </div>
+              <div className="row mt-5">
+                <div className="col-6">
+                  <div className="d-grid gap-2">
+                    <button
+                      type="button"
+                      className="btn bg-button block subtitle"
+                    >
+                      CANCELAR
+                    </button>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="d-grid gap-2">
+                    <button
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                      type="button"
+                      className="btn bg-button block subtitle"
+                    >
+                      ENVIAR
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Footer />
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="modal-body">
+              Se ha creado su usuario de administrador, se activará cuando pase
+              por las aprobaciones de las areas correspondientes Una vez creada
+              su cuenta, podrá crear usuarios secundarios a través del protal
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
