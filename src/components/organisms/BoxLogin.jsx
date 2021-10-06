0f5eadc14e42c2a33dcdb6c169b118a89975c0d2
joselo01 <sjmujica@gmail.com>
2021-10-06 09:00:16 -0500

    user por rol

--- src/components/organisms/BoxLogin.jsx
+++ src/components/organisms/BoxLogin.jsx
@@ -7,14 +7,6 @@ export const BoxLogin = () => {
       <div className="content-box-login">
         <div className="text-center">
           <div className="mb-1 title">ACCEDE AL PORTAL</div>
-          <div className="d-grid gap-2">
-            <button
-              type="submit"
-              className="btn bg-button block subtitle mb-1 mt-1"            >
-              Usuario Cliente Gloria
-            </button>
-            <div></div>
-          </div>
           <div className="mb-3 subtitle">Ingrese su usuario y contraseña</div>
         </div>
         <FormLogin />
