0f6b4b475a954366949f20962358d48702eaf04a
joselo01 <sjmujica@gmail.com>
2021-10-15 15:22:24 -0500

    add filter

--- src/components/organisms/FormPreRegistro.jsx
+++ src/components/organisms/FormPreRegistro.jsx
@@ -3,9 +3,7 @@ import { useDispatch } from "react-redux";
 import { preRegisterAddNew } from "../../redux/actions/PreRegister";
 import { useForm } from "../../hooks/useForm";
 
-
 export const FormPreRegistro = ({ handleValidated }) => {
-  
   let paises = [
     { value: "", label: "[Seleccione Pais]" },
     { value: "1", label: "Perú" },
@@ -14,7 +12,7 @@ export const FormPreRegistro = ({ handleValidated }) => {
     { value: "4", label: "Brasil" },
     { value: "5", label: "México" },
   ];
-  
+
   let empresas = [
     { value: "", label: "[Seleccione Empresa]" },
     { value: "1", label: "Empresa 1" },
@@ -23,7 +21,7 @@ export const FormPreRegistro = ({ handleValidated }) => {
     { value: "4", label: "Empresa 4" },
     { value: "5", label: "Empresa 5" },
   ];
-  
+
   let compradores = [
     { value: "", label: "[Seleccione Comprador]" },
     { value: "1", label: "Comprador 1" },
@@ -33,27 +31,27 @@ export const FormPreRegistro = ({ handleValidated }) => {
     { value: "5", label: "Comprador 5" },
   ];
 
-const [disabled, setDisabled] = useState(false);
-const [isChecked, setisChecked] = useState([]);
+  const [disabled, setDisabled] = useState(false);
+  const [isChecked, setisChecked] = useState([]);
 
-useEffect(() => {
-  let isChecked = [
-    { id: 1, value: "Rubro 1" },
-    { id: 2, value: "Rubro 2" },
-    { id: 3, value: "Rubro 3" },
-    { id: 4, value: "Rubro 4" },
-  ];
+  useEffect(() => {
+    let isChecked = [
+      { id: 1, value: "Rubro 1" },
+      { id: 2, value: "Rubro 2" },
+      { id: 3, value: "Rubro 3" },
+      { id: 4, value: "Rubro 4" },
+    ];
 
-  setisChecked(
-    isChecked.map(d => {
-      return {
-        select: false,
-        id: d.id,
-        value: d.value
-      };
-    })
-  );
-},[])
+    setisChecked(
+      isChecked.map((d) => {
+        return {
+          select: false,
+          id: d.id,
+          value: d.value,
+        };
+      })
+    );
+  }, []);
 
   const [formValues, handleInputChange] = useForm({
     idFiscal: "",
@@ -70,20 +68,21 @@ useEffect(() => {
   
 
   const handleSubmit = (e) => {
+    const rubrosFilter = isChecked.filter((item) => item.select === true);
+    console.log("rubrosFilter", rubrosFilter);
     e.preventDefault();
-    dispatch(preRegisterAddNew(
-      idFiscal, 
-      pais, 
-      isChecked, 
-      empresa, 
-      comprador, 
-      comentario
-      
-      ));
+    dispatch(
+      preRegisterAddNew(
+        idFiscal,
+        pais,
+        rubrosFilter,
+        empresa,
+        comprador,
+        comentario
+      )
+    );
     handleValidated();
   };
-
- 
 
   return (
     <form name="form" className="form-group" onSubmit={handleSubmit}>
@@ -121,29 +120,28 @@ useEffect(() => {
           <div className="multiselect">
             <div className="selectBox"></div>
             <div className="checkboxes overflow-auto">
-            {isChecked.map((d) => (
-              <div key={d.id}>
-              <span>
-                <input
-                  onChange={event => {
-                    let checked = event.target.checked;
-                    setisChecked(
-                      isChecked.map(data => {
-                        if (d.id === data.id) {
-                          data.select = checked;
-                        }
-                        return data;
-                      })
-                    );
-                  }}
-                  type="checkbox"
-                  checked={d.select}
-                ></input>
-             </span>
-             <span>{d.value}</span>
-             </div>
-          ))}
-
+              {isChecked.map((d) => (
+                <div key={d.id}>
+                  <span>
+                    <input
+                      onChange={(event) => {
+                        let checked = event.target.checked;
+                        setisChecked(
+                          isChecked.map((data) => {
+                            if (d.id === data.id) {
+                              data.select = checked;
+                            }
+                            return data;
+                          })
+                        );
+                      }}
+                      type="checkbox"
+                      checked={d.select}
+                    ></input>
+                  </span>
+                  <span>{d.value}</span>
+                </div>
+              ))}
             </div>
           </div>
         </div>
